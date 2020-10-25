035d139c1994d2b18c407d294d00cec04be85601
tigrullya <73430886+tigrullya@users.noreply.github.com>
2020-10-25 17:29:44 +0300

    Add files via upload

---
+++ lessonOne.java
@@ -0,0 +1,20 @@
+public class lessonOne {
+
+//1)
+   public static void main(String[] args) {
+        System.out.println("Hello, Tigran");
+//2)
+        byte a = 10;
+        byte b = 20;
+        int y = a + b - a;
+        System.out.println("a + b - a = " + y);
+// тоже самое с short и long, разница лишь в дозволительных объемах
+
+       String с = " I love";
+       String e = " you";
+       System.out.println("Then I said" + с + e);
+
+    }
+}
+
+
